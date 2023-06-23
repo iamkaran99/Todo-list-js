80a7cad0b60bfd1a8b8d3432c1ba646bedef2d46
iamkaran99 <104888185+iamkaran99@users.noreply.github.com>
2023-06-23 12:35:48 +0530

    Add files via upload

---
+++ To-do list/script.js
@@ -0,0 +1,43 @@
+const inputbox = document.getElementById("text");
+const listContainer = document.getElementById("listContainer");
+
+
+function addtask(){
+    if(inputbox.value === ''){
+        alert('you must write something');
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);        
+    }
+
+    inputbox.value == "";
+    savedata();
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName == "LI"){
+        e.target.classList.toggle("checked");
+        savedata();
+    }
+    else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
+        savedata();
+    }
+},false
+);
+
+
+function savedata(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showdata(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showdata();
